086f33ac05dc34cf832a13ac8f132a250b802944
Teydxx <pinel.loic@gmail.com>
2025-12-28 08:26:10 +0100

    Add files via upload

--- scripts/games.js
+++ scripts/games.js
@@ -2,21 +2,21 @@
 
 // Configuration des jeux
 const GAMES = [
-    { name: "The Legend of Zelda: Breath of the Wild", videoId: "zw47_q9wbBE" },
-    { name: "Super Mario Odyssey", videoId: "5kcdRBHM7kM" },
+    { name: "The Last of Us Part II", videoId: "lDC4X8Dgxr4" },
     { name: "God of War (2018)", videoId: "K0u_kAWLJOA" },
     { name: "Red Dead Redemption 2", videoId: "eaW0tYpxyp0" },
     { name: "The Witcher 3: Wild Hunt", videoId: "c0i88t0Kacs" },
+    { name: "Super Mario Odyssey", videoId: "5kcdRBHM7kM" },
+    { name: "Minecraft", videoId: "MmB9b5njVbA" },
+    { name: "Cyberpunk 2077", videoId: "8X2kIfS6fb8" },
     { name: "Elden Ring", videoId: "AKXiKBnzpBQ" },
     { name: "Grand Theft Auto V", videoId: "QkkoHAzjnUs" },
-    { name: "Minecraft", videoId: "MmB9b5njVbA" },
-    { name: "Fortnite", videoId: "6I_r7qJzXjA" },
-    { name: "Cyberpunk 2077", videoId: "8X2kIfS6fb8" },
     { name: "Hollow Knight", videoId: "UAO2urG23S4" },
     { name: "Portal 2", videoId: "TluRVBhmf8w" },
+    { name: "The Legend of Zelda: Breath of the Wild", videoId: "zw47_q9wbBE" },
     { name: "Dark Souls III", videoId: "c7wC8l6s8Uc" },
     { name: "Overwatch 2", videoId: "GKXS_YA9s7E" },
-    { name: "The Last of Us Part II", videoId: "lDC4X8Dgxr4" }
+    { name: "Fortnite", videoId: "6I_r7qJzXjA" }
 ];
 
 // Fonction pour mélanger un tableau
